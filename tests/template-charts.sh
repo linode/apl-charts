dbdb27b00b79f0b80eb4ba3a5dde5a0886f64bd0
merll <merll@users.noreply.github.com>
2026-09-23 11:11:16 +0200

    fix: values must be strings according to schema (#44)
    * fix: values must be strings according to schema
    
    * ci: fix chart linting script
    
    * ci: fix chart linting script
    
    ---------
    
    Co-authored-by: Jehoszafat Zimnowoda <17126497+j-zimnowoda@users.noreply.github.com>

--- tests/template-charts.sh
+++ tests/template-charts.sh
@@ -1,6 +1,12 @@
-for chart in k8s-*; do helm template $chart $chart --values=tests/test-values.yaml --output-dir  tests/output/$chart; done
-helm template knative-service --values=tests/test-values.yaml --output-dir  tests/output/knative-service
-helm template postgres-cluster --values=tests/test-values.yaml --output-dir  tests/output/postgres-cluster
-helm template pgvector-cluster --values=tests/test-values.yaml --output-dir  tests/output/pgvector-cluster
-helm template redis-cluster --values=tests/test-values.yaml --output-dir  tests/output/redis-cluster
-helm template rabbitmq-cluster --values=tests/test-values.yaml --output-dir  tests/output/rabbitmq-cluster
+set -eu
+
+charts=()
+for dir in */; do
+  if [ -f "${dir}Chart.yaml" ]; then
+    charts+=("${dir%/}")
+  fi
+done
+
+for chart in "${charts[@]}"; do
+  helm template $chart --values=tests/test-values.yaml --output-dir  tests/output/$chart
+done
